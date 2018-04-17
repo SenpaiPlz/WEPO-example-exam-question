6b094e89fb56aaa9f6f3c360f3b54aa2a26f3616
senpaiplz <senpai@plznotice.me>
2018-04-17 19:04:55 +0000

    router boilerplate ready

--- routing-boilerplate/src/App.js
+++ routing-boilerplate/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './Components/HomeScreen'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/about' render={() => <h1>About</h1>}/>
+          <Route render={() => <h1>Not Found</h1>}/>
+        </Switch>
+      </Router>
     );
   }
 }
